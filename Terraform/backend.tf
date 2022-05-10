37f2bb9e870cd547bede414844116522d9205263
vijayadurgasi <vijayadurgasi16@gmail.com>
2022-05-10 13:48:25 +1000

    Creating s3 bucket via terraform

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "mystatefilesfolder"
-    key            = "Terraform/terraform.tfstate"
+    bucket         = "dev-statefile-s3-bucket"
+    key            = "terraform.tfstate"
     region         = "ap-southeast-2"
   }
 }
---
+++ creates3/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "my-statefile-s3" {
+  bucket = "${var.environment}-statefile-s3-bucket"
+  acl    = "public-read"
+  policy = file("policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+  tags = {
+    Name        = "My bucket"
+    Environment = var.environment
+  }
+}
+
